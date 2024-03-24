154d1cf0b297d9e42143431748fd6d97a9da072a
Tim <timwagner997@gmail.com>
2024-03-24 13:22:25 +0100

    added dummy users for testing

--- dummy_data.go
+++ dummy_data.go
@@ -1,8 +1,13 @@
 package main
 
-// Some dummy Todo items
 var dummyTodos = []todo{
 	{ID: "1", Title: "Einkaufen", Text: "Ich brauch noch Toastbrot und Nutella"},
 	{ID: "2", Title: "Geschenk für Oma kaufen", Text: "Ideen: Orchidee, Pralinen, Käsekuchen"},
 	{ID: "3", Title: "Putzen", Text: "Staubsaugen, Kleiderschrank ausmisten, Schuhe putzen"},
+}
+
+var dummyUsers = []user{
+	{Username: "tim", Password: "test"},
+	{Username: "celine", Password: "mcdonalds"},
+	{Username: "ronny", Password: "benz"},
 }
