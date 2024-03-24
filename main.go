fac6359d70f36279f18f070cd34cdf1c8c48748d
Tim <timwagner997@gmail.com>
2024-03-24 21:18:27 +0100

    added instructions to connect to the MySQL Database

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
 	"github.com/tim-w97/my-awesome-Todo-API/handlers"
 	"github.com/tim-w97/my-awesome-Todo-API/middleware"
@@ -11,6 +13,41 @@ import (
 )
 
 func main() {
+	// Load environment variables from .env file
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Can't load the .env file")
+	}
+
+	// Create connection to MySQL Database
+	mySQLHost := os.Getenv("MYSQL_HOST")
+	mySQLPort := os.Getenv("MYSQL_PORT")
+	mySQLAddress := fmt.Sprintf("%s:%s", mySQLHost, mySQLPort)
+
+	mySQLConfig := mysql.Config{
+		User:   os.Getenv("MYSQL_USER"),
+		Passwd: os.Getenv("MYSQL_PASS"),
+		Net:    "tcp",
+		Addr:   mySQLAddress,
+		DBName: os.Getenv("MYSQL_DB"),
+
+		// TODO: Is this bad?
+		AllowNativePasswords: true,
+	}
+
+	db, dbError := sql.Open("mysql", mySQLConfig.FormatDSN())
+
+	if dbError != nil {
+		log.Fatal("Can't connect to the MySQL Database")
+	}
+
+	// Test the database connection
+	pingErr := db.Ping()
+
+	if pingErr != nil {
+		log.Fatal("Can't ping the MySQL Database")
+	}
+
+	// Set up routes
 	router := gin.Default()
 
 	// routes for registration and login
@@ -21,16 +58,11 @@ func main() {
 	router.GET("/todos/:id", middleware.RequireAuth, handlers.GetTodoByID)
 	router.POST("/todos", middleware.RequireAuth, handlers.AddTodo)
 
-	// load port from environment variables
-	if err := godotenv.Load(); err != nil {
-		log.Fatal("Can't load the .env file")
-	}
-
-	port := os.Getenv("PORT")
-	hostnameAndPort := fmt.Sprintf("localhost:%s", port)
-
 	// run the awesome Todo API
-	if err := router.Run(hostnameAndPort); err != nil {
+	port := os.Getenv("PORT")
+	address := fmt.Sprintf("localhost:%s", port)
+
+	if err := router.Run(address); err != nil {
 		log.Fatal("Can't run awesome Todo API:", err)
 	}
 }
