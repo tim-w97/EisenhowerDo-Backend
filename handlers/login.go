58df6ed72b42a30af0bc10393898bd567b9bba6e
Tim <timwagner997@gmail.com>
2024-03-24 15:21:20 +0100

    generate json web token with a secret from .env and send it back

--- handlers/login.go
+++ handlers/login.go
@@ -1,13 +1,25 @@
 package handlers
 
 import (
+	"errors"
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v5"
 	"github.com/tim-w97/my-awesome-Todo-API/data"
 	"github.com/tim-w97/my-awesome-Todo-API/types"
 	"net/http"
+	"os"
 	"time"
 )
+
+func searchUser(user types.User) (types.User, error) {
+	for _, dummyUser := range data.DummyUsers {
+		if dummyUser.Username == user.Username {
+			return dummyUser, nil
+		}
+	}
+
+	return user, errors.New("this user doesn't exist")
+}
 
 func Login(context *gin.Context) {
 	var requestedUser types.User
@@ -22,26 +34,20 @@ func Login(context *gin.Context) {
 		return
 	}
 
-	var foundUser types.User
+	// search user in database and return it if found
+	user, searchError := searchUser(requestedUser)
 
-	for _, dummyUser := range data.DummyUsers {
-		if dummyUser.Username == requestedUser.Username {
-			foundUser = dummyUser
-			break
-		}
-	}
-
-	if foundUser == (types.User{}) {
+	if searchError != nil {
 		context.IndentedJSON(
 			http.StatusBadRequest,
-			gin.H{"error": "This user doesn't exist"},
+			gin.H{"error": searchError.Error()},
 		)
 
 		return
 	}
 
 	// TODO: Use password hashes
-	if foundUser.Password != requestedUser.Password {
+	if user.Password != requestedUser.Password {
 		context.IndentedJSON(
 			http.StatusBadRequest,
 			gin.H{"error": "Your password is incorrect"},
@@ -53,14 +59,26 @@ func Login(context *gin.Context) {
 	// Generate the JSON Web Token and add the username to the claims
 	// The token expires after 8 hours
 	claims := jwt.MapClaims{
-		"sub": foundUser.Username,
+		"sub": user.Username,
 		"exp": time.Now().Add(time.Hour * 8).Unix(),
 	}
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	secret := []byte(os.Getenv("SECRET"))
+
+	tokenString, signError := token.SignedString(secret)
+
+	if signError != nil {
+		context.IndentedJSON(
+			http.StatusBadRequest,
+			gin.H{"error": "Can't generate token"},
+		)
+
+		return
+	}
 
 	context.IndentedJSON(
 		http.StatusOK,
-		gin.H{"jwt": token},
+		gin.H{"jwt": tokenString},
 	)
 }
