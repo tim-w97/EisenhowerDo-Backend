7f8db80e7818a086bf1758a5fac74413e1b8cab9
Tim <timwagner997@gmail.com>
2024-03-24 21:34:30 +0100

    also changed IDs in dummy data

--- data/dummy_data.go
+++ data/dummy_data.go
@@ -3,13 +3,13 @@ package data
 import "github.com/tim-w97/my-awesome-Todo-API/types"
 
 var DummyTodos = []types.Todo{
-	{ID: "1", Title: "Einkaufen", Text: "Ich brauch noch Toastbrot und Nutella"},
-	{ID: "2", Title: "Geschenk für Oma kaufen", Text: "Ideen: Orchidee, Pralinen, Käsekuchen"},
-	{ID: "3", Title: "Putzen", Text: "Staubsaugen, Kleiderschrank ausmisten, Schuhe putzen"},
+	{ID: 1, Title: "Einkaufen", Text: "Ich brauch noch Toastbrot und Nutella"},
+	{ID: 2, Title: "Geschenk für Oma kaufen", Text: "Ideen: Orchidee, Pralinen, Käsekuchen"},
+	{ID: 3, Title: "Putzen", Text: "Staubsaugen, Kleiderschrank ausmisten, Schuhe putzen"},
 }
 
 var DummyUsers = []types.User{
-	{Username: "tim", Password: "test"},
-	{Username: "celine", Password: "mcdonalds"},
-	{Username: "ronny", Password: "benz"},
+	{ID: 1, Username: "tim", Password: "test"},
+	{ID: 2, Username: "celine", Password: "mcdonalds"},
+	{ID: 3, Username: "ronny", Password: "benz"},
 }
