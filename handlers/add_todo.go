cfa3be7922243889b35d06c7dae7b0f8e1e9fdd2
Tim <timwagner997@gmail.com>
2024-03-26 20:38:43 +0100

    can no insert todos into the database table

--- handlers/add_todo.go
+++ handlers/add_todo.go
@@ -2,8 +2,9 @@ package handlers
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/tim-w97/my-awesome-Todo-API/data"
+	"github.com/tim-w97/my-awesome-Todo-API/db"
 	"github.com/tim-w97/my-awesome-Todo-API/types"
+	"log"
 	"net/http"
 )
 
@@ -11,15 +12,36 @@ func AddTodo(context *gin.Context) {
 	var newTodo types.Todo
 
 	// convert received json to a new Todo
-	if err := context.BindJSON(&newTodo); err != nil {
+	if bindErr := context.BindJSON(&newTodo); bindErr != nil {
 		context.IndentedJSON(
 			http.StatusBadRequest,
 			gin.H{"error": "Can't convert body to Todo item"},
 		)
 
+		log.Print(bindErr)
 		return
 	}
 
-	data.DummyTodos = append(data.DummyTodos, newTodo)
+	result, insertErr := db.Database.Exec(
+		"INSERT INTO todo (title, text) VALUES (?, ?)",
+		newTodo.Title,
+		newTodo.Text,
+	)
+
+	if insertErr != nil {
+		log.Print("Can't insert todo: ", insertErr)
+		context.AbortWithStatus(http.StatusInternalServerError)
+		return
+	}
+
+	insertedID, idErr := result.LastInsertId()
+
+	if idErr != nil {
+		log.Print("Can't get id of the inserted row: ", insertErr)
+		context.AbortWithStatus(http.StatusInternalServerError)
+		return
+	}
+
+	newTodo.ID = int(insertedID)
 	context.IndentedJSON(http.StatusCreated, newTodo)
 }
