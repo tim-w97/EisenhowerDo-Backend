dd96b592fb6925f9fc68b5541b8ba18de0448246
Tim <timwagner997@gmail.com>
2024-03-26 21:12:41 +0100

    Select single todos from database

--- handlers/get_todo_by_id.go
+++ handlers/get_todo_by_id.go
@@ -1,24 +1,59 @@
 package handlers
 
 import (
-	"fmt"
+	"database/sql"
+	"errors"
 	"github.com/gin-gonic/gin"
-	"github.com/tim-w97/my-awesome-Todo-API/data"
+	"github.com/tim-w97/my-awesome-Todo-API/db"
+	"github.com/tim-w97/my-awesome-Todo-API/types"
+	"log"
 	"net/http"
+	"strconv"
 )
 
 func GetTodoByID(context *gin.Context) {
-	id := context.Param("id")
+	var todo types.Todo
 
-	for _, todo := range data.DummyTodos {
-		if fmt.Sprint(todo.ID) == id {
-			context.IndentedJSON(http.StatusOK, todo)
-			return
-		}
+	idString := context.Param("id")
+
+	if idString == "" {
+		context.IndentedJSON(
+			http.StatusBadRequest,
+			gin.H{"message": "please send a todo ID"},
+		)
+
+		return
 	}
 
-	context.IndentedJSON(
-		http.StatusNotFound,
-		gin.H{"message": "todo not found"},
-	)
+	id, convertErr := strconv.Atoi(idString)
+
+	if convertErr != nil {
+		context.IndentedJSON(
+			http.StatusBadRequest,
+			gin.H{"message": "please send a numeric todo ID (examples: 1, 2, 3, 42)"},
+		)
+
+		return
+	}
+
+	row := db.Database.QueryRow("SELECT * FROM todo WHERE id = ?", id)
+
+	scanErr := row.Scan(&todo.ID, &todo.Title, &todo.Text)
+
+	if scanErr != nil {
+		if errors.Is(scanErr, sql.ErrNoRows) {
+			context.IndentedJSON(
+				http.StatusNotFound,
+				gin.H{"message": "Can't find a todo for this ID"},
+			)
+
+			return
+		}
+
+		log.Print("Can't assign todo row to todo struct: ", scanErr)
+		context.AbortWithStatus(http.StatusInternalServerError)
+		return
+	}
+
+	context.IndentedJSON(http.StatusOK, todo)
 }
