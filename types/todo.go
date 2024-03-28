a40a70a7111f56401e84fd8bb27f0d9729955752
Tim <timwagner997@gmail.com>
2024-03-28 21:33:36 +0100

    added more necessary fields to the Todo

--- types/todo.go
+++ types/todo.go
@@ -1,7 +1,9 @@
 package types
 
 type Todo struct {
-	ID    int    `json:"id"`
-	Title string `json:"title"`
-	Text  string `json:"text"`
+	ID          int    `json:"id"`
+	Title       string `json:"title"`
+	Text        string `json:"text"`
+	UserID      int    `json:"userID"`
+	IsCompleted bool   `json:"isCompleted"`
 }
