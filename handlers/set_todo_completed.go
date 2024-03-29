67c72aa3a683ff5accfe290273b3c6048229c6d9
Tim <timwagner997@gmail.com>
2024-03-29 22:30:41 +0100

    added endpoint to update the status of a todos

--- handlers/set_todo_completed.go
+++ handlers/set_todo_completed.go
@@ -1,7 +1,62 @@
 package handlers
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/tim-w97/my-awesome-Todo-API/db"
+	"github.com/tim-w97/my-awesome-Todo-API/types"
+	"log"
+	"net/http"
+)
 
 func SetTodoStatus(context *gin.Context) {
-	// TODO: implement me!
+	var todoStatus types.TodoStatus
+
+	if bindErr := context.BindJSON(&todoStatus); bindErr != nil {
+		context.IndentedJSON(
+			http.StatusBadRequest,
+			gin.H{"message": "can't read todo status from body"},
+		)
+
+		log.Print(bindErr)
+		return
+	}
+
+	result, err := db.Database.Exec(
+		"UPDATE todo SET isCompleted = ? WHERE id = ? AND userID = ?",
+		context.GetInt("todoID"),
+		context.GetInt("userID"),
+	)
+
+	if err != nil {
+		context.IndentedJSON(
+			http.StatusInternalServerError,
+			gin.H{"message": "insert failed, can't update status of todo"},
+		)
+
+		log.Print(err)
+		return
+	}
+
+	rowsAffected, err := result.RowsAffected()
+
+	if err != nil {
+		context.IndentedJSON(
+			http.StatusInternalServerError,
+			gin.H{"message": "can't count affected rows"},
+		)
+	}
+
+	if rowsAffected == 0 {
+		context.IndentedJSON(
+			http.StatusInternalServerError,
+			gin.H{"message": "updated no rows"},
+		)
+
+		return
+	}
+
+	context.IndentedJSON(
+		http.StatusOK,
+		gin.H{"message": "updated status of todo successfully"},
+	)
 }
---
+++ types/todo_status.go
@@ -0,0 +1,5 @@
+package types
+
+type TodoStatus struct {
+	IsCompleted bool `json:"isCompleted"`
+}
