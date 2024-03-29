81864c83455b9329ce0804de3c05476fe3d064f6
Tim <timwagner997@gmail.com>
2024-03-29 01:12:13 +0100

    reorganised routes with groups

--- api/endpoints.go
+++ api/endpoints.go
@@ -6,78 +6,44 @@ import (
 	"github.com/tim-w97/my-awesome-Todo-API/middleware"
 )
 
+func initLoginAndRegistration(router *gin.Engine) {
+	router.POST("/register", handlers.Register)
+	router.POST("/login", handlers.Login)
+}
+
+func initAuthorizedEndpoints(router *gin.Engine) {
+	// all routes beginning with /todos are secured with JSON Web Token Authorization
+	authorized := router.Group("/todos", middleware.JWTAuth)
+
+	// all routes with a Todo ID parameter require parsing of the todo ID
+	withTodoID := authorized.Group("/:id", middleware.ParseTodoID)
+
+	// Get all Todos of a user
+	authorized.GET("/", handlers.GetTodos)
+
+	// Get a Todo by ID
+	withTodoID.GET("/", handlers.GetTodoByID)
+
+	// Add a new Todo
+	authorized.POST("/", handlers.AddTodo)
+
+	// Share a Todo with another user
+	withTodoID.POST("/share", handlers.ShareTodo)
+
+	// Update an existing Todo
+	withTodoID.PUT("/", handlers.UpdateTodo)
+
+	// Change the list position of a Todo
+	withTodoID.PUT("/position", handlers.ChangeTodoPosition)
+
+	// Toggle a Todo as completed or uncompleted
+	withTodoID.PUT("/status", handlers.SetTodoStatus)
+
+	// Delete a Todo
+	withTodoID.DELETE("/", handlers.DeleteTodo)
+}
+
 func initEndpoints(router *gin.Engine) {
-	// Registration and Login
-
-	router.POST(
-		"/register",
-		handlers.Register,
-	)
-
-	router.POST(
-		"/login",
-		handlers.Login,
-	)
-
-	// Getting Todo items
-
-	router.GET(
-		"/todos",
-		middleware.JWTAuth,
-		handlers.GetTodos,
-	)
-
-	router.GET(
-		"/todos/:id",
-		middleware.JWTAuth,
-		middleware.ParseTodoID,
-		handlers.GetTodoByID,
-	)
-
-	// Add and share Todo items
-
-	router.POST(
-		"/todos",
-		middleware.JWTAuth,
-		handlers.AddTodo,
-	)
-
-	router.POST(
-		"/todos/:id/share",
-		middleware.JWTAuth,
-		middleware.ParseTodoID,
-		handlers.ShareTodo,
-	)
-
-	// Alter Todo items
-
-	router.PUT(
-		"/todos/:id",
-		middleware.JWTAuth,
-		middleware.ParseTodoID,
-		handlers.UpdateTodo,
-	)
-
-	router.PUT(
-		"/todos/:id/position",
-		middleware.JWTAuth,
-		middleware.ParseTodoID,
-		handlers.ChangeTodoPosition,
-	)
-
-	router.PUT(
-		"/todos/:id/status",
-		middleware.JWTAuth,
-		middleware.ParseTodoID,
-		handlers.SetTodoStatus,
-	)
-
-	// Delete Todo items
-
-	router.DELETE(
-		"/todos/:id",
-		middleware.JWTAuth,
-		middleware.ParseTodoID,
-		handlers.DeleteTodo,
-	)
+	initLoginAndRegistration(router)
+	initAuthorizedEndpoints(router)
 }
