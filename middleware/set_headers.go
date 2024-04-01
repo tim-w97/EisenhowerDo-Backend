b674b4997f43e1be6cad969e655a3ad38ff93855
Tim <timwagner997@gmail.com>
2024-04-01 14:34:52 +0200

    added allowed methods + return with status 204 if method is OPTIONS

--- middleware/set_headers.go
+++ middleware/set_headers.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
 	"os"
 )
 
@@ -12,9 +13,19 @@ func SetHeaders(context *gin.Context) {
 	)
 
 	context.Header(
+		"Access-Control-Allow-Methods",
+		"GET, POST, PUT, DELETE, OPTIONS",
+	)
+
+	context.Header(
 		"Access-Control-Allow-Credentials",
 		"true",
 	)
+
+	// Handle preflight requests
+	if context.Request.Method == "OPTIONS" {
+		context.Status(http.StatusNoContent)
+	}
 
 	// Continue with the request if everything goes well
 	context.Next()
