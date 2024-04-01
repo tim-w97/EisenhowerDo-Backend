5158511fbb2548ca384f912a032e3eff5519fb7a
Tim <timwagner997@gmail.com>
2024-04-01 13:20:30 +0200

    replace * wildcard with value from .env

--- middleware/set_headers.go
+++ middleware/set_headers.go
@@ -2,23 +2,18 @@ package middleware
 
 import (
 	"github.com/gin-gonic/gin"
+	"os"
 )
 
 func SetHeaders(context *gin.Context) {
 	context.Header(
 		"Access-Control-Allow-Origin",
-		//os.Getenv("WEBSITE"),
-		"*",
+		os.Getenv("WEBSITE"),
 	)
 
 	context.Header(
 		"Access-Control-Allow-Credentials",
 		"true",
-	)
-
-	context.Header(
-		"Content-Type",
-		"application/json; charset=utf-8",
 	)
 
 	// Continue with the request if everything goes well
