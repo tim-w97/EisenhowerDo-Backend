36ad2353310778116376ccd6216fc003b631ca5f
Tim <timwagner997@gmail.com>
2024-04-03 16:58:56 +0200

    sql reformat

--- sql/endpoints/get_todos.sql
+++ sql/endpoints/get_todos.sql
@@ -1,5 +1,5 @@
 SELECT todo.id, userID, title, text, categoryID, position, isCompleted
 FROM todo
-LEFT JOIN sharedTodo
-ON todo.id = sharedTodo.todoID
+    LEFT JOIN sharedTodo
+    ON todo.id = sharedTodo.todoID
 WHERE ? IN (userID, otherUserID);
